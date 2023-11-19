da315bcfdf3d99acbfd21e9671806e12de0f4da6
heiler <threepebbles@naver.com>
2023-11-19 10:12:03 +0900

    Test: 테스트 클래스 네이밍 변경

--- src/test/java/christmas/view/InputDtoConverterTest.java
+++ src/test/java/christmas/view/InputDtoConverterTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-public class InputDtoValidationTest {
+public class InputDtoConverterTest {
     private InputDtoConverter inputDtoConverter;
 
     @BeforeEach
